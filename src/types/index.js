cc8bd729948603399ab42cb8abf20b210ab99296
drake.aren <drake.aren@protonmail.com>
2019-01-26 02:56:48 +0600

    add type index page

---
+++ src/types/index.js
@@ -0,0 +1,9 @@
+const Transaction = require('./transaction')
+const Block = require('./block')
+const Blockchain = require('./blockchain')
+
+module.exports = {
+  Transaction,
+  Block,
+  Blockchain
+}
