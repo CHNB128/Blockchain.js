16579f4310eb654b201c1912b2c137df40f7766c
drake.aren <drake.aren@protonmail.com>
2019-01-30 16:26:30 +0600

    set fixed standart db port

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 // Connection URL
-const url = 'mongodb://localhost:32769/blockchain'
+const url = 'mongodb://localhost:27001/blockchain'
 // Use connect method to connect to the server
 module.exports = function(callback) {
   MongoClient.connect(
